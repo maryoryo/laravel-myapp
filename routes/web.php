f8a89328b8c06f1edb5f4f2e20436f9c5b7c6663
liyomaxxde@gmail.com <liyomaxxde@gmail.com>
2021-10-09 17:48:18 +0000

    laravel dev

--- routes/web.php
+++ routes/web.php
@@ -16,33 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', function () {
-    return view('index');
-});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/', function () {
-    return view('index');
-});
+require __DIR__.'/auth.php';
